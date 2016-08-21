ba1172d44b01808f0b54a6c308f4ceff08fbdc78
Konrad Pozniak <k.pozniak@gmx.at>
2016-08-21 12:23:33 +0200

    moved favicon to default root location

--- src/templates/header.html.php
+++ src/templates/header.html.php
@@ -5,7 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta name="robots" content="index, nofollow" />
 <title> <?php echo $site_name; ?> </title>
-<link rel="icon" type="image/x-icon" href="<?php echo $server_path; ?>/assets/favicon.ico" />
+<link rel="icon" type="image/x-icon" href="<?php echo $server_path; ?>/favicon.ico" />
 <?php if(isset($canonical_url)) echo "<link rel=\"canonical\" href=\"$canonical_url\">"; ?>
 <link rel="stylesheet" type="text/css" href="<?php echo $server_path; ?>/assets/style.css" />
 </head>
