80d7f0999e09349cf2291107da35b785a275415c
Konrad Pozniak <k.pozniak@gmx.at>
2016-09-24 17:32:20 +0200

    removed unneeded stuff from redirect.html.php template

--- src/templates/redirect.html.php
+++ src/templates/redirect.html.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html><head>
 <title><?php echo $site_name; ?></title>
-<meta name="referrer" content="no-referrer" />
-<meta http-equiv="Refresh" content="0; url=<?php echo htmlspecialchars($url, ENT_COMPAT | ENT_HTML5, "UTF-8", false); ?>" />
+<meta name="referrer" content="no-referrer"/>
+<meta http-equiv="Refresh" content="0; url=<?php echo htmlspecialchars($url, ENT_COMPAT | ENT_HTML5, "UTF-8", false); ?>"/>
 <script type="text/javascript">
 window.location.replace( <?php echo json_encode($url, JSON_HEX_QUOT|JSON_HEX_TAG|JSON_HEX_AMP|JSON_HEX_APOS); ?>);
 </script>
 </head>
-<body><p>Redirecting to <a rel="nofollow" href="<?php echo htmlspecialchars($url, ENT_COMPAT | ENT_HTML5, "UTF-8", false); ?>"><?php echo htmlspecialchars($url, ENT_COMPAT | ENT_HTML5, "UTF-8", false); ?></a></p></body></html>
+<body>Redirecting to <a rel="nofollow" href="<?php echo htmlspecialchars($url, ENT_COMPAT | ENT_HTML5, "UTF-8", false); ?>"><?php echo htmlspecialchars($url, ENT_COMPAT | ENT_HTML5, "UTF-8", false); ?></a></body></html>
